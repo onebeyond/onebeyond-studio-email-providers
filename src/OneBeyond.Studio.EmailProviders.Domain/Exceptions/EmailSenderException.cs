c7064d4c11ecb45ed78dde4dbf7a85e316f8e098
Andrii Kaplanovskyi <andrii.kaplanovskyi@dcslsoftware.com>
2023-11-14 19:09:22 +0000

    .net 8 migration bug fix

--- src/OneBeyond.Studio.EmailProviders.Domain/Exceptions/EmailSenderException.cs
+++ src/OneBeyond.Studio.EmailProviders.Domain/Exceptions/EmailSenderException.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Runtime.Serialization;
 
 namespace OneBeyond.Studio.EmailProviders.Domain.Exceptions;
 
@@ -29,15 +28,6 @@ public sealed class EmailSenderException : Exception
     /// <param name="innerException"></param>
     public EmailSenderException(string message, Exception innerException)
         : base(message, innerException)
-    {
-    }
-
-    /// <summary>
-    /// </summary>
-    /// <param name="info"></param>
-    /// <param name="context"></param>
-    private EmailSenderException(SerializationInfo info, StreamingContext context)
-        : base(info, context)
     {
     }
 }
